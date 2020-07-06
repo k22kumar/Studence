e1869b3aa4ade3d6183a1c1d28174e1da5b61b6f
Kajanth Kumar <kajanthkumar@gmail.com>
2020-07-06 10:07:19 -0400

    added styling for ad listings

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Ad = (props) => {
     return (
-        <Link to={`/fullAd/${props.ad.id}`} onClick={() => props.getSelectedAd(props.ad)}>
-            <div>
+        <Link className="flexParent adListing" to={`/fullAd/${props.ad.id}`} onClick={() => props.getSelectedAd(props.ad)}>
+            <div className="flexParent adPicContainer">
                 {
                     // if ther is no pic than put default no image available
                     props.ad.picture === "" ?
@@ -12,7 +12,7 @@ const Ad = (props) => {
                     <img src={props.ad.picture} alt={props.ad.title} />
                 }
             </div>
-            <div>
+            <div className="adListingText">
                 <h3>Title: {props.ad.title}</h3>
                 <p>Price: ${props.ad.price}</p>
             </div>
--- src/components/AdBoard.js
+++ src/components/AdBoard.js
@@ -3,7 +3,8 @@ import Ad from './Ad';
 
 const AdBoard = (props) => {
     return (
-        <div>
+        <div className="flexParent flexColumn adBoard">
+            <h2>Listings</h2>
             {
                 props.ads.map((ad, index) => {
                     return <Ad key={index} ad={ad} getSelectedAd={props.getSelectedAd}/>;
